287562e1bcf093d8b86bfc9ffda3997b6bcf9fdf
vedikaaa101 <vedikasupatkar@gmail.com>
2026-09-13 23:20:17 +0530

    Update database connection details in db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,13 +1,13 @@
 <?php
-$host = "sql202.infinityfree.com";
-$username = "if0_42711587";
-$password = "Your_password"";
-$database = "if0_42711587_jobtracker";
+
+$host = "localhost";
+$username = "root";
+$password = "YOUR_PASSWORD_HERE";
+$database = "careerflow";
 
 $conn = new mysqli($host, $username, $password, $database);
 
 if ($conn->connect_error) {
-    die("Database connection failed: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
-
-$conn->set_charset("utf8mb4");
+?>
